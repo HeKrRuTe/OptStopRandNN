bug fix in the computation of rho (greek); greek PDE method for gamma now works with dividends; binomial pricing model now automatically works for multidimensional put options (by only specifying nb_stocks instead of changing sigma and dividend) -> config was adjusted accordingly

optimal_stopping/algorithms/backward_induction/backward_induction_pricer.py
@@ -381,11 +381,14 @@ class AmericanOptionPricer:
     disc_factor = np.math.exp((-self.model.rate) * self.model.maturity /
                               self.model.nb_dates)
     rate_old = copy.copy(self.model.rate)
+    drift_old = copy.copy(self.model.drift)
     self.model.rate += eps
+    self.model.drift += eps
     stock_paths_p, var_paths_p = self.model.generate_paths(dW=dW)
     disc_factor_p = np.math.exp((-self.model.rate) * self.model.maturity /
                                 self.model.nb_dates)
     self.model.rate = rate_old
+    self.model.drift = drift_old
     time_path_gen = time.time() - t1
 
     price, rho = self.get_forward_derivative(
@@ -400,15 +403,19 @@ class AmericanOptionPricer:
     disc_factor = np.math.exp((-self.model.rate) * self.model.maturity /
                               self.model.nb_dates)
     rate_old = copy.copy(self.model.rate)
+    drift_old = copy.copy(self.model.drift)
     self.model.rate += eps
+    self.model.drift += eps
     stock_paths_p, var_paths_p = self.model.generate_paths(dW=dW)
     disc_factor_p = np.math.exp((-self.model.rate) * self.model.maturity /
                                 self.model.nb_dates)
     self.model.rate = rate_old - eps
+    self.model.drift = drift_old - eps
     stock_paths_m, var_paths_m = self.model.generate_paths(dW=dW)
     disc_factor_m = np.math.exp((-self.model.rate) * self.model.maturity /
                                 self.model.nb_dates)
     self.model.rate = rate_old
+    self.model.drift = drift_old
     time_path_gen = time.time() - t1
 
     price, rho = self.get_central_derivative(
@@ -541,7 +548,8 @@ class AmericanOptionPricer:
     if self.model.name == "BlackScholes":
       return utilities.compute_gamma_via_BS_PDE(
         price=price, delta=delta, theta=theta, rate=self.model.rate,
-        vola=self.model.volatility, spot=self.model.spot)
+        vola=self.model.volatility, spot=self.model.spot,
+        dividend=self.model.dividend)
     return None
 
   def get_regression(self, spot, eps, d, dW):

optimal_stopping/algorithms/finite_differences/binomial.py
@@ -3,15 +3,11 @@ from math import exp, sqrt
 import numpy as np
 import tqdm
 import copy
+import optimal_stopping.payoffs.payoff as po
 
 import optimal_stopping.algorithms.utils.utilities as utilities
 
 
-
-
-def put(spot, strike):
-  """Returns put for given spot and strike."""
-  return max(strike-spot, 0)
 
 class BinomialPricer:
   def __init__(self, model, payoff, **kwargs):
@@ -24,15 +20,25 @@ class BinomialPricer:
     vol = self.model.volatility
     nb_dates = self.model.nb_dates
     rate = self.model.rate
-    drift = self.model.drift
-
+    nb_stocks = self.model.nb_stocks
     payoff_fct = self.payoff
+    dividend = self.model.dividend
+
+    if nb_stocks > 1 and isinstance(payoff_fct, po.Put1Dim):
+      vol_hat = vol/np.sqrt(nb_stocks)
+      dividend_hat = dividend + vol**2/2 - vol_hat**2/2
+    elif nb_stocks == 1:
+      vol_hat = vol
+      dividend_hat = dividend
+    else:
+      raise ValueError("Binomial Pricer is not implemented for nb_stocks>1 if "
+                       "the payoff is not Put")
 
     deltaT = maturity / nb_dates
     discount_factor = exp(-rate * deltaT)
-    up = exp(vol * sqrt(deltaT))
+    up = exp(vol_hat * sqrt(deltaT))
     down = 1 / up
-    proba_up = (exp(drift * deltaT) - down) / (up - down)
+    proba_up = (exp((rate - dividend_hat) * deltaT) - down) / (up - down)
     proba_down = 1 - proba_up
     steps = range(nb_dates)
     #  ---- OLD (SLOW) VERSION ----
@@ -153,9 +159,23 @@ class BinomialPricer:
 
   def compute_gamma_via_PDE(self, price, delta, theta):
     if self.model.name == "BlackScholes":
+      vol = self.model.volatility
+      nb_stocks = self.model.nb_stocks
+      payoff_fct = self.payoff
+      dividend = self.model.dividend
+      if nb_stocks > 1 and isinstance(payoff_fct, po.Put1Dim):
+        vol_hat = vol/np.sqrt(nb_stocks)
+        dividend_hat = dividend + vol**2/2 - vol_hat**2/2
+      elif nb_stocks == 1:
+        vol_hat = vol
+        dividend_hat = dividend
+      else:
+        raise ValueError("Binomial Pricer is not implemented for nb_stocks>1 "
+                         "if the payoff is not Put")
       return utilities.compute_gamma_via_BS_PDE(
         price=price, delta=delta, theta=theta, rate=self.model.rate,
-        vola=self.model.volatility, spot=self.model.spot)
+        vola=vol_hat, spot=self.model.spot,
+        dividend=dividend_hat)
     return None
 
   def price_and_greeks(

optimal_stopping/run/configs.py
@@ -209,55 +209,47 @@ table_smallDim_BS_GeoPut_BS = _VerySmallDimensionTable(
 table_smallDim_BS_GeoPut_ref1 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
     nb_stocks=[1], nb_dates=[10000],
-    volatilities=[0.2], dividends=[0.0],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig2 = 0.2*np.sqrt(5)/5
 table_smallDim_BS_GeoPut_ref2 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig2], dividends=[0.2**2/2 - sig2**2/2],
+    nb_stocks=[5], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig3 = 0.2*np.sqrt(10)/10
 table_smallDim_BS_GeoPut_ref3 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig3], dividends=[0.2**2/2 - sig3**2/2],
+    nb_stocks=[10], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig4 = 0.2*np.sqrt(20)/20
 table_smallDim_BS_GeoPut_ref4 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig4], dividends=[0.2**2/2 - sig4**2/2],
+    nb_stocks=[20], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig5 = 0.2*np.sqrt(50)/50
 table_smallDim_BS_GeoPut_ref5 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig5], dividends=[0.2**2/2 - sig5**2/2],
+    nb_stocks=[50], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig6 = 0.2*np.sqrt(100)/100
 table_smallDim_BS_GeoPut_ref6 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig6], dividends=[0.2**2/2 - sig6**2/2],
+    nb_stocks=[100], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig7 = 0.2*np.sqrt(500)/500
 table_smallDim_BS_GeoPut_ref7 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig7], dividends=[0.2**2/2 - sig7**2/2],
+    nb_stocks=[500], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig8 = 0.2*np.sqrt(1000)/1000
 table_smallDim_BS_GeoPut_ref8 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig8], dividends=[0.2**2/2 - sig8**2/2],
+    nb_stocks=[1000], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
-sig9 = 0.2*np.sqrt(2000)/2000
 table_smallDim_BS_GeoPut_ref9 = _VerySmallDimensionTable(
     payoffs=['Put1Dim'], algos=["B"], nb_runs=1,
-    nb_stocks=[1], nb_dates=[10000],
-    volatilities=[sig9], dividends=[0.2**2/2 - sig9**2/2],
+    nb_stocks=[2000], nb_dates=[10000],
+    volatilities=[0.2],
     stock_models=['BlackScholes'],)
 # -- overview table
 table_GeoPut_payoffs_gt1 = _DimensionTable(